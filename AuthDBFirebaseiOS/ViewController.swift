c4124ae7d4421cad21b7a67c05718dbf3ea4b1cd
josuesf <113552@unsaac.edu.pe>
2019-03-25 20:36:34 -0500

    Initial Commit

---
+++ AuthDBFirebaseiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthDBFirebaseiOS
+//
+//  Created by Josue Farfan Lazo on 3/25/19.
+//  Copyright © 2019 Josue Farfan Lazo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
