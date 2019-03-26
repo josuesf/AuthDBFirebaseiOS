141a69919aec88b523bd78eabc708eb86a0f58d2
josuesf <113552@unsaac.edu.pe>
2019-03-25 22:54:46 -0500

    first commit

---
+++ AuthDBFirebaseiOS/Controllers/HomeController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthDBFirebaseiOS
+//
+//  Created by Josue Farfan Lazo on 3/25/19.
+//  Copyright © 2019 Josue Farfan Lazo. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+    }
+    
+    
+}
+
---
+++ Pods/Target Support Files/Pods-AuthDBFirebaseiOS/Pods-AuthDBFirebaseiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AuthDBFirebaseiOS : NSObject
+@end
+@implementation PodsDummy_Pods_AuthDBFirebaseiOS
+@end
